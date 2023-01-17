26ba56c56ec9e173513e2692682d8b6430e64e5d
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-01-17 10:41:25 -0600

    Saving token into the browser

--- src/redux/localStorage/storage.js
+++ src/redux/localStorage/storage.js
@@ -2,9 +2,11 @@
 /* eslint-disable max-len */
 import loadingStatus from '../reduxConst';
 
+const LOCAL_STORAGE_NAME = 'token';
+
 const loadState = () => {
   try {
-    const serializedData = localStorage.getItem('state');
+    const serializedData = localStorage.getItem(LOCAL_STORAGE_NAME);
 
     if (serializedData === null) {
       console.log('Local Storage null');
@@ -24,21 +26,20 @@ const loadState = () => {
 
 const saveState = (state) => {
   try {
-    if (state.User.loading === loadingStatus.succeeded) {
-      const serializedData = JSON.stringify(state.User);
-      console.log('State updated on the Local Storage');
-      localStorage.setItem('state', serializedData);
-    }
+    // if (state.User.loading === loadingStatus.succeeded) {
+    //   // const serializedData = JSON.stringify(state.User);
+    //   console.log('State for the User information');
+    //   // localStorage.setItem('state', serializedData);
+    // }
     if (state.Auth.loading === loadingStatus.succeeded) {
-      console.log('LOG OUT OR ENTRY');
-      console.log('Auth state:', state.Auth);
+      console.log('Save token into the localStorage as "token"');
+      // console.log('Auth state:', state.Auth.user.token);
+      const token = JSON.stringify(state.Auth.user.token);
+      localStorage.setItem(LOCAL_STORAGE_NAME, token);
     }
     console.log(state.Auth.loading);
-    // const serializedData = JSON.stringify(state.User);
-    // console.log('State updated on the Local Storage');
-    // localStorage.setItem('state', serializedData);
   } catch (error) {
-    console.log('Error saving information:', error);
+    console.log('Error saving information into the Local Storage:', error);
   }
 };
 
