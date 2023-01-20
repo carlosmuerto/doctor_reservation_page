b134c8d71ccbfafb6e06c55f4c342be823c3b050
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-01-19 18:47:55 -0600

    clean up

--- src/redux/URL_API.js
+++ src/redux/URL_API.js
@@ -1,3 +1,7 @@
+// Connect this project to the API
 // const BASEURL = 'https://doctor-reservation-api-k121.onrender.com/';
+
+// Local version
 const BASEURL = 'http://localhost:3000/';
+
 export default BASEURL;
